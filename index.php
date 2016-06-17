734b5660a5c1ab81a94db5a8ae0f71f1a95b3f0d
Evgeniy.Ruslanovich <evgeniy.zaynutdinov@gmail.com>
2016-06-17 15:01:15 +0400

    Добавлены хитрые мат. операции. Заголовки заменены на более маленькие. (лучше бы это разбить на два коммита)

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 ﻿<?php
-    echo "Hello world!";
-    
+    echo "Hello world!"; 
 ?>
 
-
+<h2>Разбираем примеры из методички</h2>
 <?php
+
     $name = "Mahmud";
-    echo "<h1>variables</h1>Hello, $name!";
+    echo "<h3>variables</h3>Hello, $name!";
 ?>
 <?php
     $a = "Elefant";
     $b = true;
     $c = 22;
     $d = 45.4;
-    echo "<h1> type of variables</h1>string: $a ! <br>";
+    echo "<h3> type of variables</h3>string: $a ! <br>";
     echo "boolean: $b <br>";
     echo "number: $c <br>";
     echo "float: $d <hr>";
 ?>
 <?php
-     echo "<h1>String tricks</h1>";
+     echo "<h3>String tricks</h3>";
     $a = 'Die ';
     $b = "hard";
     $c = "$a, $b";
@@ -28,14 +28,14 @@
     echo " $d  <hr>";
 ?>
 <?php
-     echo "<h1>Concatination</h1>";
+     echo "<h3>Concatination</h3>";
     $a = 'Everybody';
     $b = 'dance';
     $c = $a . " " . $b;
     echo " $c  <hr>";
 ?>
 <?php
-     echo "<h1>Ghost to type</h1>";
+     echo "<h3>Ghost to type</h3>";
     $a = (int)'05';
     $b = (string)1148.4;
     $c = (bool)-4;
@@ -44,7 +44,7 @@
     echo 'number -> bool -> string: ' . $c . '<hr>';
 ?>
 <?php
-    echo "<h1>Math ops</h1>";
+    echo "<h3>Math ops</h3>";
     $a = 6;
     $b = 4;
     echo 'Сложение 6+4' . ($a + $b) . '<br>';
@@ -53,4 +53,13 @@
     echo 'Деление 6/4=' . $a / $b . '<br>';
     echo 'Остаток от деления ост(6/4)=' . $a % $b . '<br>';
     echo 'Возведение в степень 6^4=' . pow($a, $b) . '<hr>';
+  //  echo 'Возведение в степень 6^4=' . $a ** $b . '<hr>';
+?>
+<?php
+    echo "<h3>Tricky math ops</h3>";
+    $a = 6;
+    $b = 4;
+    $a += $b;
+    echo 'a=6, b=4, a += b <br>';
+    echo 'a=' . $a . '<hr>';
 ?>
