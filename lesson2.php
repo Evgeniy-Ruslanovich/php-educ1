f009d03b8dfe55ab8cf815b3d8f955a1f5a73ee4
Evgeniy.Ruslanovich <evgeniy.zaynutdinov@gmail.com>
2016-06-18 20:41:56 +0400

    Докручен алгоритм, выдащий разное приветствие в зависимости от времени суток.

--- lesson2.php
+++ lesson2.php
@@ -5,17 +5,18 @@
  *страница выводит разное приветствие в зависимости от времени суток,
  *и предложения по меню, в зависимости от кулинарных предпочтений клиента
  */
+
     $userName = 'Василий'; //имя пользователя
     $time =  (int) date (G); //Запрашиваем время, сразу переводим в целое число
 
 
-    echo "<h1> Привет,  $userName!</h1>";
-    //echo "$time<br>";  Проверка как запросилось время
-    //echo gettype($time); проверка, какого типа в итоге время
+ /**   echo "<h1> Привет,  $userName!</h1>";
+ *  //echo "$time<br>";  Проверка как запросилось время
+ * //echo gettype($time); проверка, какого типа в итоге время
+*
+ *   $dayPeriod = 0; //переменая времени дня
 
-    $dayPeriod = 0; //переменая времени дня
-
-  /**  swith ($time)
+/**  swith ($time)
         case ($time < 6)
             $dayPeriod = 0;
             break;
@@ -28,17 +29,57 @@
             $dayPeriod = 0;
             break;
      */
-  function getDayPeriod ($clock)
-    if ($time <=6) {
-        $dayPeriod = 1;
-    } elseif ($time >= 6 && $time < 12) {
-        $dayPeriod = 2;
-    } elseif ($time >= 12 && $time < 18) {
-        $dayPeriod = 3;
-    } elseif ($time >= 18 && $time < 24) {
-        $dayPeriod = 4;
+/**
+ * Функция возвращает цифру в зависимости от вермени суток.
+ * до 6 утра - 1. С 6 до 12 - 2, с 12 до 18 - 3, с 18 до 24 - 4.
+ * @return int
+ */
+    function getDayPeriod ()
+    {
+        Global $time;
+        if ($time <= 6) {
+          return 1;
+        } elseif ($time >= 6 && $time < 12) {
+          return 2;
+        } elseif ($time >= 12 && $time < 18) {
+          return 3;
+        } elseif ($time >= 18 && $time < 24) {
+          return 4;
+        }
     }
-   // echo "$dayPeriod"; проверяем, как определилось время дня
 
+    $dayPeriod = getDayPeriod (); //присваиваем значение периода дня с помощью функции
+
+  //  echo "$dayPeriod <br>"; //проверяем, как определилось время дня
+    $Hello = "Доброго времени суток, "; //дефаультное значение переменной "приветственная фраза", на случай если что-то пойдет не так
+/**
+ * Функция присваивает переменной приветственой фразы $Hello разное значение, в зависимости от времени суток
+ * @param $Hello
+ */
+    function helloPhrase (&$Hello)
+    {
+        Global $dayPeriod;
+        switch ($dayPeriod) {
+            case 1:
+              $Hello = "Доброй ночи, ";
+                break;
+            case 2:
+                $Hello = "Доброй утро, ";
+                break;
+            case 3:
+                $Hello = "Добрый день, ";
+                break;
+            case 4:
+                $Hello = "Добрый вечер, ";
+                break;
+            default:
+                break;
+        }
+
+    }
+helloPhrase ($Hello); //Вызываем функцию присвоения приветственной фразы
+// echo "$Hello <br>"; проверка приветственной фразы
+
+echo "<h1> $Hello $userName!</h1>";
 
    ?>
