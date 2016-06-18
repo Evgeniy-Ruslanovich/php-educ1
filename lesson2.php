83a690ff466ff67fbdff95f33e7fcdd0e1055058
Evgeniy.Ruslanovich <evgeniy.zaynutdinov@gmail.com>
2016-06-18 20:10:58 +0400

    Добавлено ветвление, определяющее время суток. 1 - ночь, 2 утро, 3 день, 4 вечер

--- lesson2.php
+++ lesson2.php
@@ -13,6 +13,32 @@
     //echo "$time<br>";  Проверка как запросилось время
     //echo gettype($time); проверка, какого типа в итоге время
 
-    swith 
+    $dayPeriod = 0; //переменая времени дня
+
+  /**  swith ($time)
+        case ($time < 6)
+            $dayPeriod = 0;
+            break;
+        case ($time >= 6)&&($time < 12)
+            $dayPeriod = 2;
+        case 0,1,2,3,4,5
+            $dayPeriod = 0;
+            break;
+        case 0,1,2,3,4,5
+            $dayPeriod = 0;
+            break;
+     */
+  function getDayPeriod ($clock)
+    if ($time <=6) {
+        $dayPeriod = 1;
+    } elseif ($time >= 6 && $time < 12) {
+        $dayPeriod = 2;
+    } elseif ($time >= 12 && $time < 18) {
+        $dayPeriod = 3;
+    } elseif ($time >= 18 && $time < 24) {
+        $dayPeriod = 4;
+    }
+   // echo "$dayPeriod"; проверяем, как определилось время дня
+
 
    ?>
