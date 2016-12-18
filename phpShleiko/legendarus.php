1b02fac6593ad42d9494c831d22933ec16b559c8
EvgeniyZ <evgeniy.zaynutdinov@gmail.com>
2016-12-18 22:27:35 +0400

    Переместил файлики

---
+++ phpShleiko/legendarus.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 07.12.2016
+ * Time: 20:40
+ */
---
+++ phpShleiko/lesson3/lesson3-1.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 10.12.2016
+ * Time: 16:43
+ */
+
